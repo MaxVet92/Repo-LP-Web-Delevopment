b2770082157df734375535d01be827411987216a
Max Vetterer <mvetterer@gmail.com>
2026-01-19 10:41:21 +0100

    Added remaining requirements

--- exercise2.py
+++ exercise2.py
@@ -1,6 +1,6 @@
 
 def amount():
-    amount = int(input("How much money would you like to convert?"))
+    amount = int(input("How much money would you like to convert? "))
     while True:
         if amount < 0:
             print("This is not a valid amount. Please try again")
@@ -8,30 +8,38 @@ def amount():
         break
     return amount
 
-def Currency_Source():
-
-    print("What is your source currency?")
-    print("EUR, USD, or RUB")
+def supported_currencies():
+    view_currencies = input("Would you like to view supported currencies? Type 'Y' or 'N'. ")
+    currencies = ("EUR", "USD", "RUB")
     while True:
-        source_choice = input("Choose source currency (1/2/3): ")
+        if view_currencies == "Y":
+            print(currencies)  
+            break
+        elif view_currencies == "N":
+            break 
+        else:
+            print("Invalid input. Please try again")
+    return
+
+def Currency_Source():
+    while True:
+        source_choice = input("Choose source currency. ")
         if source_choice in ("EUR", "USD", "RUB"):
             break
-        print("Invalid choice. Please select 1, 2, or 3.")
+        print("Invalid choice. Please select 'EUR', 'USD' or 'RUB'")
     return source_choice
 
 def Currency_Target(source_choice):
-
-    print("What is your target currency")
-    print("EUR, USD, or RUB")
     while True:
-        target_choice = input("Choose target currency (1/2/3): ")
+        target_choice = input("Choose target currency. ")
         if target_choice in ("EUR", "USD", "RUB") and target_choice != source_choice:
             break
-        print("Invalid choice. Please select 1, 2, or 3 and a differnt currency than the source.")
+        print("Invalid choice. Please select 'EUR', 'USD' or 'RUB' and a differnt currency than the source.")
     return target_choice
 
 ### Currency
 amount = int(amount())
+supported_currencies()
 source = Currency_Source()
 target = Currency_Target(source)
 
@@ -52,9 +60,14 @@ current_rate = rates[(source, target)]
 def change_exchange_rate(rates, source, target):
     current_rate = rates[(source, target)]
     while True:
-        change_Y_or_N = input(f"This is the current exchange rates: {exchange_rate}. Would you like to change it? Answer Y or N")
+        change_Y_or_N = input(f"This is the current exchange rates: {current_rate}. Would you like to change it? Answer Y or N. ")
         if change_Y_or_N == "Y":
-            new_rate = float(input(f"You want to change the excange rate from {source} to {target} from {exchange_rate} to?"))
+            while True:
+                new_rate = float(input(f"You want to change the excange rate from {source} to {target}. The old rate was {current_rate}. What shall be the new exchange rate? "))
+                if new_rate <= 0:
+                    print("Invalid input. The new exchange rate must be bigger than 0. Please try again")
+                else:
+                    break
             rates[(source, target)] = new_rate
             return new_rate
         elif change_Y_or_N == "N":
